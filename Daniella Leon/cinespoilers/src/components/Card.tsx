fabf9c2218e3d2bcdb2d868c24967babba9a7792
Daniella <daniella.leon@tecsup.edu.pe>
2026-05-24 14:40:41 -0500

    Estados en Componente - evidencias 1,2,3 y 4

---
+++ Daniella Leon/cinespoilers/src/components/Card.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+const Card = ({ title, description }: CardProps) => {
+  const [likes, setLikes] = useState(0);
+
+
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <button className="button">
+        Likes {likes}
+      </button>
+    </div>
+  );
+};
+
+export default Card; 
